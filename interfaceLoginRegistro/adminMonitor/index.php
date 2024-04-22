ca5d114306657a94f1463243c0749949bbf1f91d
JadielsonAlb <jadielsonalbapolinario@gmail.com>
2024-04-21 22:24:02 -0300

    interfaceLoginRegistro 3.2
    fiz uma condição que se voce não é admin ele automaticamente irá impedir de acessar
    
    mudei o nome do modelo 360 para admin 360

--- interfaceLoginRegistro/adminMonitor/index.php
+++ interfaceLoginRegistro/adminMonitor/index.php
@@ -1,4 +1,17 @@
 <?php 
 
-header('Location: 360Alunos.php')
+session_start();
+include_once('AulaEad/script.php');
+
+
+if ($_SESSION['usuario'] === 'admin@@@') {
+    header('Location: 360Alunos.php');
+} else {
+    header("Location: ../sair.php");
+    unset($_SESSION['usuario']);
+    unset($_SESSION['senha']);
+}
+
+
+
 ?>
